fa0ba67f9e1f62f3608c98939bc26915dccccc2e
nicolastaf <nicolas.staffent@gmail.com>
2023-01-26 18:18:09 +0100

    Add bootstrap

---
+++ src/assets/js/main.js
@@ -0,0 +1,6 @@
+// Dans main js on centralise toutes les ressources JS/CSS
+
+import '../sass/style.scss';
+// Import all of Bootstrap's CSS
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
